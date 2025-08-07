dbaf6aa74a4b0fb0fe87ec952d4718c21f8782b2
Ludovic Georges <Ludovic@MacBookPro.lan>
2025-08-07 10:52:27 +0200

    Ajout render.yaml et build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
-#!/usr/bin/env bash
-pip install -r requirements.txt
-playwright install --with-deps
+#!/bin/bash
+apt-get update && apt-get install -y wget curl gnupg
+apt-get install -y libnss3 libatk-bridge2.0-0 libgtk-3-0 libxss1 libasound2
+pip install playwright
+python -m playwright install
